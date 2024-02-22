ec85c9e5b2444a1795a8b28d2d0d726b61ad759f
Alena Iadkouskaya <koptewka@mail.ru>
2024-02-22 20:50:32 +0100

    resolve conflict

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -2,12 +2,17 @@ package org.example.model;
 
 public abstract class User {
     private Integer id;
-    private String name;
-    private String surname;
+    protected String name;
+    protected String surname;
 
     public User(Integer id, String name, String surname) {
         this.id = id;
         this.name = name;
         this.surname = surname;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + surname;
     }
 }
